4166a460ee8dde2a9d0c95d5bc34b161fc5faf49
David <davidchu1236@gmail.com>
2022-10-22 20:21:21 -0500

    starting proyect

---
+++ src/main/java/com/system/growth_system/GrowthSystemApplication.java
@@ -0,0 +1,13 @@
+package com.system.growth_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowthSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowthSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/growth_system/GrowthSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.growth_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowthSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
